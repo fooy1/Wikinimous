5caafc8055b8c99e591e9c408d12e56e32c002aa
Oyvind Fossum <fooy1@pm.me>
2021-11-09 16:10:31 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 10 posts in the database
+puts 'Creating 10 fake posts...'
+5.times do
+  @articles = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+  )
+  @articles.save!
+end
